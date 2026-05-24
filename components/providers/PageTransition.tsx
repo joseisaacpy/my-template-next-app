5a3562f02cba89db2a572aad612bfc1e97389f59
José Isaac <joseisaacnascimento@gmail.com>
2026-05-24 16:16:49 -0300

    feat(animations): add pathname key to PageTransition

--- components/providers/PageTransition.tsx
+++ components/providers/PageTransition.tsx
@@ -3,10 +3,18 @@
 import { motion } from "motion/react";
 
 import { pageTransition } from "@/components/animations/index";
+import { usePathname } from "next/dist/client/components/navigation";
 
 export function PageTransition({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
+
   return (
-    <motion.div variants={pageTransition} initial="hidden" animate="visible">
+    <motion.div
+      variants={pageTransition}
+      key={pathname}
+      initial="hidden"
+      animate="visible"
+    >
       {children}
     </motion.div>
   );
